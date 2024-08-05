59c03220aa065c87a8bbf28befedba7d0875f0a5
cy <luichiyan0101@gmail.com>
2024-08-05 10:07:43 +0800

    Done~~

--- app/src/main/java/com/example/assignment1/HomePage.java
+++ app/src/main/java/com/example/assignment1/HomePage.java
@@ -1,6 +1,5 @@
 package com.example.assignment1;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,9 +7,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
